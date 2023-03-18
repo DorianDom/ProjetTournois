51e2b222d1eb4e08a2e7c552f37e57903c9ff92e
DorianDom <domengedorian@gmail.com>
2023-03-18 12:20:37 +0100

    Update pool.js

--- src/services/pools.js
+++ src/services/pools.js
@@ -1,3 +1,18 @@
 export const createPools = (players) => {
-    return [[...players]]
+    let pool=null
+    if(players.length<=5){
+        let p1=[players]
+        return p1
+    }
+    else if(players.length=6){
+        pool=[players[0,1,2],players[3,4,5]]
+        return pool
+    }
+    else if(players.length=7){
+        return pool[players[0,1,2,3],players[4,5,6]]
+    }
+    else if(players.length=8){
+        return pool[players[0,1,2,3],players[4,5,6,7]]
+    }
+    else return null
 }
