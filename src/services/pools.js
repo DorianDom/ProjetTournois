0cbcdcc1093422c283d62fcd6e1f2eb8b952c540
DorianDom <domengedorian@gmail.com>
2023-03-21 09:19:23 +0100

    Update pools.js

--- src/services/pools.js
+++ src/services/pools.js
@@ -1,26 +1,16 @@
 export const createPools = (players) => {
-    let pool=null
-    if(players.length<=5){
-        let p1=[players]
-        return p1
+    const playersPerPool = 4;
+    const numPools = Math.ceil(players.length / playersPerPool);
+    const pools = [];
+    for (let i = 0; i < numPools; i++) {
+        pools.push([]);
     }
-    else if(players.length=6){
-        let p1=[players[0],players[1],players[2]]
-        let p2=[players[3],players[4],players[5]]
-        pool=[p1,p2]
-        return pool
+    
+    for (let i = 0; i < players.length; i++) {
+        const player = players[i];
+        const poolIndex = i % numPools;
+        pools[poolIndex].push(player);
     }
-    else if(players.length=7){
-        let p1=[players[0],players[1],players[2]]
-        let p2=[players[3],players[4],players[5],players[6]]
-        pool=[p1,p2]
-        return pool
-    }
-    else if(players.length=8){
-        let p1=[players[0],players[1],players[2],players[3]]
-        let p2=[players[4],players[5],players[6],players[7]]
-        pool=[p1,p2]
-        return pool
-    }
-    else return null
+    return pools;
+    
 }
